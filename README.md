6e7beaae377f00bd95f39b2f4960528f219bf025
Prophet Daniel <prophetdaniel@ethereumclassic.org>
2018-04-21 00:16:30 -0300

    :memo: Improve README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+# GitHub Release Downloader
+
+## How to install
+```bash
+npm install github-release-downloader
+```
+
+## How to use
+```javascript
+import {GetLatestGitHubReleaseInfo} from 'github-release-downloader';
+require('babel-polyfill');
+require('babel-register');
+
+(async () => {
+  let info = await uGetLatestGitHubReleaseInfo("ethereumproject/go-ethereum");
+  console.log(info.releaseName + " was updated " + info.timeAgo + " and downloaded " + info.downloadCount.toLocaleString() + " times.",);
+  console.log(info.downloadUrl);
+})();
+```
--- test/test.js
+++ test/test.js
@@ -1,17 +1,11 @@
 var assert = require('assert');
 import {GetLatestGitHubReleaseInfo} from "../src/GetLatestGitHubReleaseInfo"
 
-// (async () => {
-//   let info = await GetLatestGitHubReleaseInfo("ethereumproject/go-ethereum");
-//   $(".download").attr("href", info.downloadUrl);
-//   $(".release-info").text(info.releaseInfo);
-//   $(".release-info").fadeIn("slow");
-// })();
-
 describe('Array', function() {
   it('should get repo info', async function() {
     let info = await GetLatestGitHubReleaseInfo("ethereumproject/go-ethereum")
     assert(info.releaseName.startsWith('Ethereum Classic Geth'))
-    console.log(info.downloadUrl)
+    console.log(info.releaseName + " was updated " + info.timeAgo + " and downloaded " + info.downloadCount.toLocaleString() + " times.")
+    console.log(info.downloadUrl);
   });
 });
